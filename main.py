280a2c86397d61d5562ff0732e1efe0728cd8ac5
Kim Hyunju <hjyg010s@gmail.com>
2025-06-16 05:04:33 +0900

    파일명 변경

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def resource_path(relative_path):
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
-form = resource_path("PDF2IMG.ui")
+form = resource_path("main.ui")
 form_class = uic.loadUiType(form)[0]
 
 class MyWindow(QMainWindow, form_class):
